65cd5812046d430e0a677d0cab1d749dd9405dfe
Jay <jay@mobilelegalsolutions.com>
2024-01-14 08:49:13 -0800

    followed course, made my own changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,68 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.deepOrangeAccent, // Body background color
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(
+            child: Text('Simple Dice', style: TextStyle(color: Colors.white)),
+          ),
+          backgroundColor: Colors.black, // Top bar color
         ),
         body: DicePage(),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.black, // Bottom bar color
+          child: Container(height: 50.0), // Adjust the height as needed
+        ),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              }, // Empty function for onPressed
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              }, // Empty function for onPressed
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
